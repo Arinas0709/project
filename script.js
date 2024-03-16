1a3e84a0a9b6f9563eb2a5fa1980f921ce428605
Arinas <swetlichnayaalinne@yandex.ru>
2024-03-16 16:57:46 +0300

    twotext

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 "use strict";
+const numberOfFilms= +prompt("Сколько фильмов вы уже посмотрели?","");
+const personalMovieDB={
+      count:numberOfFilms,
+     movies:{},
+     actors:{},
+     genres:[],
+     private:false
 
-// alert('Hello');
-// const result=confirm("Are you here");
-// console.log(result);
-// const answer=prompt("Вам есть 18?","18");
-// console.log(typeof(answer));
-// const answers =[];
-// answers[0]= prompt('Как ваше имя?','');
-// answers[1]= prompt('Фамилия','');
-// answers[2]= prompt('Сколько лет?','');
-// document.write(answers);
-const category ='toys';
+};
+
+const a= prompt("Один из последних просмотренных фильмов?",""),
+      b= prompt("На сколько оцените его?",""),
+      c= prompt("Один из последних просмотренных фильмов?",""),
+      d= prompt("На сколько оцените его?","");
+personalMovieDB.movies[a]=b;
+personalMovieDB.movies[c]=d;
+console.log(personalMovieDB);
+
